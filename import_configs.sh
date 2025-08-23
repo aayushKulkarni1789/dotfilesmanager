3ec2843326f6955cc0111c7fe6acc590919f537c
Aayush Kulkarni <aayushmkulkarni.5678@gmail.com>
2025-08-23 12:33:00 +0530

    added auto stow on importing configs

--- import_configs.sh
+++ import_configs.sh
@@ -2,10 +2,10 @@
 
 DOTFILES_DIR="dotfiles"
 
-echo "NOTE: This will replace the existing configs in your system with the one in the ./$DOTFILES_DIR/ directory. It is recommended to take a backup first."
-read -p="PROCEED? [Yy]" choice
+echo "==> NOTE: This will replace the existing configs in your system with the one in the ./$DOTFILES_DIR/ directory. It will also stow the imported configs automatically. It is recommended to take a backup first."
+read -p="-> PROCEED? [Yy]" choice
 case $choice in
-  [yY]* ) echo "Proceeding...";;
+  [yY]* ) echo "-> Proceeding...";;
   *) exit ;;
 esac
 
@@ -13,6 +13,9 @@ cd ./$DOTFILES_DIR/
 directories_to_choose=$(fd --base-directory="$PWD" -H -t d -d 1| fzf -m --prompt="SELECT PACKAGES TO INSTALL >> ")
 
 for directory in $directories_to_choose; do
+  echo "==> Importing $directory..."
   cp -av "$PWD/$directory." "$HOME/"
+  stow -R -t "$HOME" --adopt $directory
+  echo "\n\n"
 done
 
